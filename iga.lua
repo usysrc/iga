8d369c17a5718d884fbea4869a29571d4701d70d
usysrc <usysrc@usysrc.dev>
2021-04-20 18:27:47 +0200

    ported to love 11.3

--- iga.lua
+++ iga.lua
@@ -39,7 +39,7 @@ function iga:getColours()
 	return colours
 end
 
-function iga:geninit(img, ow,oh,iw,ih,N_)
+function iga:geninit(imgdata, ow,oh,iw,ih,N_)
 	-- Dimensions of the output/input are given by MMF2
 	output_width = ow
 	output_height = oh
@@ -80,13 +80,13 @@ function iga:geninit(img, ow,oh,iw,ih,N_)
 			colours[id] = 0
 		end
 	end
-	local data = img:getData()
+	local data = imgdata
 	for i=0,input_width-1 do
 		for j=0,input_height-1 do
 			local id = i + j * input_width
 			-- MF_getrgb fetches the colour of a given pixel of the input image from MMF's side
 			local r,g,b = data:getPixel(i,j)
-			local colour = string.format("%03d%03d%03d",r,g,b)
+			local colour = string.format("%03d%03d%03d",math.floor(r*255),math.floor(g*255),math.floor(b*255))
 			colour = tonumber(colour)
 			input[id] = colour
 			
--- main.lua
+++ main.lua
@@ -4,11 +4,12 @@ love.window.setMode(800,600,{borderless = true})
 
 math.randomseed(os.time())
 
-img = love.graphics.newImage("input/input_10.png")
+local imgdata = love.image.newImageData("input/input_10.png")
+
 
 local ow, oh = 100,100
 
-iga:geninit(img, ow,ow, img:getWidth(), img:getHeight(), 3)
+iga:geninit(imgdata, ow,ow, imgdata:getWidth(), imgdata:getHeight(), 3)
 
 love.draw = function()
 	love.graphics.scale(8,8)
@@ -23,7 +24,7 @@ love.draw = function()
 			if k and k > 0 then
 				local c = string.format("%09d",tostring(k))
 				local r,g,b = string.sub(c,1, 3), string.sub(c,4, 6), string.sub(c,7, 9)
-				love.graphics.setColor(tonumber(r),tonumber(g),tonumber(b))
+				love.graphics.setColor(tonumber(r)/255,tonumber(g)/255,tonumber(b)/255)
 				love.graphics.rectangle("fill",x,y,1,1)
 			end
 		end
